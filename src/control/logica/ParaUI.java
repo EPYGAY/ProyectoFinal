afa9a4727eb9651a06d3235d2ab7f7174e0ff5eb
sergiogalan99 <sergiocalamonte2018@gmail.com>
2019-06-05 21:45:15 +0200

    Cambios

--- src/control/logica/ParaUI.java
+++ src/control/logica/ParaUI.java
@@ -2,20 +2,11 @@ package control.logica;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.ArrayList;
-
-import javax.swing.ComboBoxModel;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JComboBox;
-import javax.swing.JTextField;
 
 import facade.Facade;
-import vista.comunes.PanelDatosPersonales;
-import vista.comunes.PanelMensaje;
+
 import vistaUI.UI;
 import vistas.controlador.ControladorMensaje;
 import vistas.controlador.ControladorPanelDatosPersonales;
@@ -34,6 +25,7 @@ public class ParaUI extends UI {
 				facade.guardarPaciente(controladorPanelDatosPersonales.obtenerDatos(getPanelDatosPersonalesAltaPaciente()));
 				controladorPanelDatosPersonales.vaciarDatos(altaPaciente.getPanelDatosPersonales());
 				controladorMensaje.mostrarMensajes(altaPaciente.getPanelMensaje(), "Paciente registrado correctamente");
+				
 			}
 		});
 		
@@ -41,14 +33,7 @@ public class ParaUI extends UI {
 		getComboBoxIdConsultaPaciente().addFocusListener(actionComboPacienteConsulta);
 		getComboBoxIdConsultaPaciente().addItemListener(actionComboPacienteConsulta);
 		
-		bajaPaciente.panelMensaje.getBtnAplicr().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				facade.darBajaPaciente(controladorPanelDatosPersonales.obtenerDatos(getPanelDatosPersonalesBajaDoctor()));
-				controladorPanelDatosPersonales.vaciarDatos(bajaPaciente.getPanelDatosPersonales());
-				controladorMensaje.mostrarMensajes(bajaPaciente.getPanelMensaje(), "Paciente dado de baja correctamente");
-			}
-		});
-		ActionComboPaciente actionComboPacienteBaja=new ActionComboPaciente(getComboBoxIdBajaPaciente(), facade, controladorPanelDatosPersonales, getPanelDatosPersonalesBajaDoctor());
+		ActionComboPaciente actionComboPacienteBaja=new ActionComboPaciente(getComboBoxIdBajaPaciente(), facade, controladorPanelDatosPersonales, getPanelDatosPersonalesBajaPaciente());
 		getComboBoxIdBajaPaciente().addFocusListener(actionComboPacienteBaja);
 		getComboBoxIdBajaPaciente().addItemListener(actionComboPacienteBaja);
 		
@@ -57,17 +42,31 @@ public class ParaUI extends UI {
 				facade.modificarPaciente(controladorPanelDatosPersonales.obtenerDatos(getPanelDatosPersonalesModificacionPaciente()));
 				controladorPanelDatosPersonales.vaciarDatos(modificacionPaciente.getPanelDatosPersonales());
 				controladorMensaje.mostrarMensajes(modificacionPaciente.getPanelMensaje(), "Paciente modificado correctamente");
+				
 			}
 		});
 		ActionComboPaciente actionComboPacienteModificacion=new ActionComboPaciente(getComboBoxIdModificacionPaciente(), facade, controladorPanelDatosPersonales, getPanelDatosPersonalesModificacionPaciente());
 		getComboBoxIdModificacionPaciente().addFocusListener(actionComboPacienteModificacion);
 		getComboBoxIdModificacionPaciente().addItemListener(actionComboPacienteModificacion);
-	
+		
+		bajaPaciente.panelMensaje.getBtnAplicr().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				facade.darBajaPaciente(controladorPanelDatosPersonales.obtenerDatos(getPanelDatosPersonalesBajaPaciente()));
+				controladorPanelDatosPersonales.vaciarDatos(bajaPaciente.getPanelDatosPersonales());
+				controladorMensaje.mostrarMensajes(bajaPaciente.getPanelMensaje(), "Paciente dado de baja correctamente");
+				
+			}
+		});
+		
+			
+		
+		
 		//PACIENTES 
 		altaMedico.panelMensaje.btnAplicr.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				facade.guardarDoctor(controladorPanelDatosPersonales.obtenerDatos(getPanelDatosPesonalesAltaDoctor()));
 				controladorPanelDatosPersonales.vaciarDatos(altaMedico.getPanelDatosPersonales());
+				controladorMensaje.mostrarMensajes(altaMedico.getPanelMensaje(), "Doctor dado de alta correctamente");
 			}
 		});
 		ActionComboDoctor actionComboDoctorConsulta=new ActionComboDoctor(getComboBoxIdConsultaDoctor(), facade, controladorPanelDatosPersonales, getPanelDatosPersonalesConsultaDoctor());
@@ -80,12 +79,17 @@ public class ParaUI extends UI {
 		
 		bajaMedico.panelMensaje.btnAplicr.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				facade.darBajaDoctor(controladorPanelDatosPersonales.obtenerDatos(getPanelDatosPesonalesBajaDoctor()));
+				facade.darBajaDoctor(controladorPanelDatosPersonales.obtenerDatos(getPanelDatosPersonalesBajaDoctor()));
 				controladorPanelDatosPersonales.vaciarDatos(bajaMedico.getPanelDatosPersonales());
+				controladorMensaje.mostrarMensajes(bajaMedico.getPanelMensaje(), "Doctor dado de baja correctamente");
 			}
 		});
 		
-		
 	}
 
+
+	
+	
+	
+	
 }
